7aa782579759de35ddb4287b3c3595916433c214
akempel <anna.kb.kempel@gmail.com>
2024-09-23 18:19:51 -0400

    Successfully renders plant cards to the DOM from array returned from API call

--- client/containers/SearchedPlantsContainer.jsx
+++ client/containers/SearchedPlantsContainer.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import SearchedPlantCard from '../components/SearchedPlantCard';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { setLoggedIn } from '../reducers/userSlice';
 
 const SearchedPlantsContainer = () => {
+  const [plants, setPlants] = useState([]);
   const currentResults = useSelector((state) => state.search.currentResults);
-  const plantResults = [];
   // iterate over currentResults array
   // for each item in currentResults, create a new SearchedPlantCard component
   useEffect(() => {
+    const plantResults = [];
     for (let i = 0; i < currentResults.length; i++) {
       plantResults.push(
         <SearchedPlantCard
@@ -17,8 +20,14 @@ const SearchedPlantsContainer = () => {
         />
       );
     }
+    setPlants(plantResults);
   }, [currentResults]);
-  return <div id='searched-plants-container'>{plantResults}</div>;
+  return (
+    <div id='searched-plants-container'>
+      <p>Results:</p>
+      {plants}
+    </div>
+  );
 };
 
 export default SearchedPlantsContainer;
