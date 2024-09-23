6328611b17d393a946f6d64b6381a4410fa65023
David <davidjs314@yahoo.com>
2024-09-23 11:25:51 -0400

    Builds out user and plant models
    Co-authored-by: timothychengg <timothychengg@users.noreply.github.com>

---
+++ server/models/plantModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const plant = mongoose.Schema({
+  plantId: { type: Number, required: true, unique: true },
+  commonName: { type: String, required: true },
+  watering: { type: String,},
+  // frontend should be aware that this is an object and will have to create a deep clone
+  wateringGeneralBenchMark: { type: Object},
+  sunlight: { type: Array},
+  careGuide: { type: String},
+  careLevel: { type: String},
+  poisonousToPet: { type: Boolean},
+  poisonousToHuman: { type: Boolean},
+  imageUrl: { type: String},
+  description: { type: String},
+  indoor: { type: Boolean}
+});
+
+
+
+// You must export your model through module.exports
+
+module.exports = mongoose.model('plant', plant);
