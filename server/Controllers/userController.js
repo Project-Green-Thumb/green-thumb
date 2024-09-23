42ff3610df8e8f384653d24edb63bd6aecd06b23
David <davidjs314@yahoo.com>
2024-09-23 13:23:06 -0400

    Add's middleware for signingup and logging in
    Co-authored-by: timothychengg <timothychengg@users.noreply.github.com>
    Co-authored-by: Andrew Mott <andrew-mott@users.noreply.github.com>

---
+++ server/Controllers/userController.js
@@ -0,0 +1,53 @@
+const User = require('../models/userModel');
+
+const userController = {};
+
+userController.createUser = async (req, res, next) => {
+  const { username, password } = req.body
+  
+    if(!username || !password){
+        return next(
+            {
+                message: 'Both username and password fields are required'
+            });
+    }
+
+  try {
+   const newUser = await User.create({username: username, password: password});
+   console.log(newUser);
+   return next();
+  }catch (error) {
+    console.log(error);
+    return next({message: 'error creating account! username may be taken'});
+  }
+};
+
+userController.verifyUser = async (req, res, next) => {
+const { username, password } = req.body;
+
+if(!username || !password){return  next({message: 'both username and password fields are required'});}
+
+try {
+  const user = await User.findOne({username: username});
+  if(user === null){return next({message: 'Username or Password was incorrect'})};
+  if(password === user.password){
+    console.log(user);
+    console.log('login verifyed');
+    return next()}
+  else{
+    return next({message: 'Username or Password was incorrect'});
+  }
+  
+}catch (error) {
+  console.log(error);
+    return next({message: 'Username or Password was incorrect'});
+}
+
+};
+
+module.exports = userController;
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ const path = require('path');//requires in path for better path's
 //body-parser
 const app = express();//makes a express app
 const mongoose = require('mongoose');//requires in mongoose
+const userController = require(path.join(__dirname, './Controllers/userController.js'))
 
 mongoose.connect('mongodb://127.0.0.1:27017/green-thumb')
 .then( (result) => {
-    console.log('Mongoose DB connected', result);
+    //console.log('Mongoose DB connected', result);
 })
 .catch((err) => {
     console.log('Failed to connect to the Mongoose DB', err);
@@ -18,12 +19,12 @@ app.use(express.json());
 
 //router handlers
 //create user document in user model
-app.post('/api/signup', (req, res) => {
-
+app.post('/api/signup', userController.createUser, (req, res) => {
+    res.sendStatus(200);
 });
 //find matching username and verifying password, potentially creating a session
-app.get('/api/login', (req, res) => {
-
+app.get('/api/login', userController.verifyUser, (req, res) => {
+    res.sendStatus(200);
 });
 
 //fetch user's saved favorites
@@ -45,6 +46,13 @@ app.get('/api/species/details', (req, res) => {
 
 });
 
+
+//global error handler
+app.use((err, req, res, next) => {
+    console.log(err);
+     const errObj = Object.assign({message: 'Unknown erro caught by error handler'} ,err)
+    res.status(500).json(errObj);
+});
 
 
 
