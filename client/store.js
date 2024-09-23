aea3ec449d583a48850bbb1e16bdd685d7b35e21
akempel <anna.kb.kempel@gmail.com>
2024-09-23 15:35:26 -0400

    Adds new search state and reducer. Adds in MVP functionality to reducer cases in search and user reducers.

--- client/store.js
+++ client/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/userSlice';
+import searchReducer from './reducers/searchSlice';
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    search: searchReducer,
   },
 });
