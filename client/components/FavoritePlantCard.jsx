a731c2fbfce578bf6cc5221395b4f96b4a7708d1
akempel <anna.kb.kempel@gmail.com>
2024-09-23 12:23:31 -0400

    Renames previous component files to .jsx, and adds a few more basic components like plant cards and the login form

---
+++ client/components/FavoritePlantCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavoritePlantCard = () => {
+  return (
+    <div>FavoritePlantCard</div>
+  )
+}
+
+export default FavoritePlantCard
---
+++ client/containers/SearchedePlantsContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchedePlantsContainer = () => {
+  return (
+    <div>SearchedePlantsContainer</div>
+  )
+}
+
+export default SearchedePlantsContainer
